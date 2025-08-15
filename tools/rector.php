46ede29dd75658281c64a9442ea7bfed72f30e3d
Abdouni Abdelkarim <abdounikarim@gmail.com>
2025-08-15 03:11:48 +0200

    Update rector configuration file

--- tools/rector.php
+++ tools/rector.php
@@ -1,30 +1,38 @@
 <?php
 
-declare(strict_types=1);
-
 use Rector\Config\RectorConfig;
-use Rector\Core\ValueObject\PhpVersion;
 use Rector\Doctrine\Set\DoctrineSetList;
-use Rector\Set\ValueObject\SetList;
-use Rector\Symfony\Set\SymfonySetList;
 
-return static function (RectorConfig $rectorConfig): void {
-    $rectorConfig->phpVersion(PhpVersion::PHP_83);
-
-    $rectorConfig->paths([
+return RectorConfig::configure()
+    ->withPaths([
         __DIR__.'/../config',
-        __DIR__.'/../public',
+        __DIR__.'/../migrations',
         __DIR__.'/../src',
         __DIR__.'/../tests',
-    ]);
-
-    $rectorConfig->sets([
-        SetList::DEAD_CODE,
-        SetList::EARLY_RETURN,
-        SetList::PHP_83,
-        SetList::TYPE_DECLARATION,
+    ])
+    ->withSkip([
+        __DIR__.'/../config/bootstrap.php',
+        __DIR__.'/../config/bundles.php',
+        __DIR__.'/../config/preload.php',
+        __DIR__.'/../public/index.php',
+        __DIR__.'/../src/Kernel.php',
+        __DIR__.'/../tests/bootstrap.php',
+    ])
+    ->withPreparedSets(
+        deadCode: true,
+        codeQuality: true,
+        codingStyle: true,
+        typeDeclarations: true,
+        privatization: true,
+        naming: true,
+        earlyReturn: true,
+        rectorPreset: true,
+    )
+    ->withSets([
+        DoctrineSetList::ANNOTATIONS_TO_ATTRIBUTES,
         DoctrineSetList::DOCTRINE_CODE_QUALITY,
-        DoctrineSetList::DOCTRINE_ORM_214,
-        SymfonySetList::SYMFONY_CODE_QUALITY,
-    ]);
-};
+    ])
+    ->withComposerBased(symfony: true)
+    ->withAttributesSets(symfony: true)
+    ->withPhpSets(php84: true)
+;
