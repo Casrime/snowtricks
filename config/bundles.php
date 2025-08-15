e15ecd32aaeff1722d4d86fd577e5927707b528e
Abdouni Abdelkarim <abdounikarim@gmail.com>
2025-08-15 03:31:24 +0200

    Install symfonycasts/tailwind-bundle

--- config/bundles.php
+++ config/bundles.php
@@ -15,4 +15,5 @@ return [
     DAMA\DoctrineTestBundle\DAMADoctrineTestBundle::class => ['test' => true],
     Symfony\UX\StimulusBundle\StimulusBundle::class => ['all' => true],
     Symfony\UX\Turbo\TurboBundle::class => ['all' => true],
+    Symfonycasts\TailwindBundle\SymfonycastsTailwindBundle::class => ['all' => true],
 ];
