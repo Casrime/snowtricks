011a1b1dc50997887b984db82bde88039c29f057
Abdouni Abdelkarim <abdounikarim@gmail.com>
2025-10-02 08:25:28 +0200

    Remove symfony/asset-mapper

--- assets/app.js
+++
@@ -1,6 +0,0 @@
-import './bootstrap.js';
-import './vendor/flowbite/dist/flowbite.turbo.js';
-import './js/dark-mode.js';
-import './js/toggle-dark-mode.js';
-import './styles/app.css'
-import './styles/header.css'
